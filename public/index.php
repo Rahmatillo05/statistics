ade58c5da0b4b038ee2cbe7665f70490d62f7aa6
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-04 21:59:55 +0500

    Add view section

--- public/index.php
+++ public/index.php
@@ -8,4 +8,4 @@ require_once __DIR__ . "/../vendor/autoload.php";
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 $view = new View(dirname(__DIR__));
-$view->render('index');
+echo $view->render('index');
