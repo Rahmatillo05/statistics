a6defbb9940afa2fcb621a8c00f9042fc7cf72ee
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-04 21:31:48 +0500

    Add debt class

--- public/index.php
+++ public/index.php
@@ -6,4 +6,3 @@ use Dotenv\Dotenv;
 require_once __DIR__."/../vendor/autoload.php";
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
-require __DIR__."/../src/main.php";
---
+++ src/Debt.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\core;
+
+class Debt extends Money
+{
+    public function dailyNewDebt(): array
+    {
+
+        $debt = (int)self::$db
+            ->database
+            ->sum('debt_history',
+                'debt_amount',
+                [
+                    'created_at[<>]' => [self::$START_DAY, self::$END_DAY]
+                ]
+            );
+        $paid = (int)self::$db
+            ->database
+            ->sum('debt_history',
+                'pay_amount',
+                [
+                    'created_at[<>]' => [self::$START_DAY, self::$END_DAY]
+                ]
+            );
+        $remaining = $debt - $paid;
+
+        return compact('debt', 'paid', 'remaining');
+    }
+
+    public function paymentHistory(): int
+    {
+        return (int)self::$db
+            ->database
+            ->sum('payment_history_list',
+                'pay_amount',
+                [
+                    'created_at[<>]' => [self::$START_DAY, self::$END_DAY]
+                ]
+            );
+    }
+
+    public function dailyDebt(): array
+    {
+        $old_debt = (int)self::$db
+            ->database->sum(
+                'old_debt',
+                'amount',
+                [
+                    'created_at[<>]' => [self::$START_DAY, self::$END_DAY]
+                ]
+            );
+        $all_debt = $this->dailyNewDebt()['debt'] + $old_debt;
+        $paid = $this->paymentHistory() + $this->dailyNewDebt()['paid'];
+        $remaining = $all_debt - $paid;
+        return compact('all_debt', 'paid', 'remaining');
+
+    }
+
+}
