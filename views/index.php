05fd44d12f967b53dec2f3076196183d30da38fa
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-05 17:38:01 +0500

    Add view section

--- views/index.php
+++ views/index.php
@@ -1,3 +1,49 @@
-<h1>
-    Hello world
-</h1>
+<div class="row mt-5">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h3 class="card-title">Umumiy holat</h3>
+                    <form action="/sorting">
+                        <h4>Sana bo'yicha ko'rish:</h4>
+                        <label>
+                            Boshlanish sanasi
+                            <input type="date" name="Sorting[start]" class="form-control">
+                        </label>
+                        <label>
+                            Tugash sanasi
+                            <input type="date" class="form-control" name="Sorting[end]">
+                        </label>
+                        <button type="submit" class="btn btn-primary">Ko'rish</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table table-responsive">
+                    <table class="table">
+                        <tr class="text-center">
+                            <th colspan="6"><h5>Naqd pul</h5></th>
+                        </tr>
+                        <tr>
+                            <th>Sotishdan</th>
+                            <th>Aralash sotish</th>
+                            <th>Qarzni to'lash</th>
+                            <th>Qarzga sotilgandagi to'lov</th>
+                            <th colspan="2"><h6>Umumiy</h6></th>
+                        </tr>
+                        <tr class="text-center">
+                            <th colspan="6"><h5>Plastikka</h5></th>
+                        </tr>
+                        <tr>
+                            <th>Sotishdan</th>
+                            <th>Aralash sotish</th>
+                            <th>Qarzni to'lash</th>
+                            <th>Qarzga sotilgandagi to'lov</th>
+                            <th colspan="2"><h6>Umumiy</h6></th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
